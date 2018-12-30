2f26cf5f2e7ab7eb792effd8fcada10965c57699
Guillaume Pelletier-Auger <pelletierauger@gmail.com>
2018-12-30 16:29:36 -0500

    Add functionality for sending OSC messages from SuperCollider to p5.js

--- server.js
+++ server.js
@@ -12,6 +12,24 @@ var htmlSources = {
 
 let sketchFolder, sketchName, sketchIndex;
 
+var getIPAddresses = function() {
+    var os = require("os"),
+        interfaces = os.networkInterfaces(),
+        ipAddresses = [];
+
+    for (var deviceName in interfaces) {
+        var addresses = interfaces[deviceName];
+        for (var i = 0; i < addresses.length; i++) {
+            var addressInfo = addresses[i];
+            if (addressInfo.family === "IPv4" && !addressInfo.internal) {
+                ipAddresses.push(addressInfo.address);
+            }
+        }
+    }
+
+    return ipAddresses;
+};
+
 if (!process.argv[2]) {
     console.log("A sketch name must be provided.");
     return;
@@ -94,6 +112,25 @@ var udpPort = new osc.UDPPort({
     remotePort: 57120,
     metadata: true
 });
+
+udpPort.on("ready", function() {
+    var ipAddresses = getIPAddresses();
+    console.log("Listening for OSC over UDP.");
+    ipAddresses.forEach(function(address) {
+        console.log(" Host:", address + ", Port:", udpPort.options.localPort);
+    });
+});
+
+udpPort.on("message", function(oscMessage) {
+    // console.log(oscMessage);
+    io.sockets.emit('receiveOSC', oscMessage);
+    // console.log(JSON.parse(oscMessage));
+});
+
+udpPort.on("error", function(err) {
+    console.log(err);
+});
+
 udpPort.open();
 
 var sclang;
