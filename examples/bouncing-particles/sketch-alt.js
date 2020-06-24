8d1d6a83d8c3f50bc7bddd3d7a8286a87cb3d811
Guillaume Pelletier-Auger <pelletierauger@gmail.com>
2020-06-24 19:53:47 -0400

    Work on a first example

---
+++ examples/bouncing-particles/sketch-alt.js
@@ -0,0 +1,127 @@
+// When a p5.js sketch first runs, it evaluates all the code files
+// that it contains. But you may want to store code for the specific
+// purpose of live coding - code that you do not want the sketch to
+// evaluate automatically.
+
+// One method to do this is used below:
+
+// You can nest code within an if statement that will always
+// be false in order to create blocks that will not be evaluated
+// when the sketch first runs. You are then free to 
+// evaluate them whenever you want, with command-Enter.
+// Below, we can evaluate a new draw() function to change
+// the behaviour of the particles.
+
+// An alternative method would be to simply comment out code blocks 
+// that you want to prevent from being automatically evaluated.
+// But the "always false" if statement method allows you to evaluate 
+// your code blocks without changing anything in the code - and it
+// also communicates the intent very clearly.
+
+if (false) {
+
+    draw = function() {
+        background(180, 200, 200);
+        var notesToSend = 0;
+        // The first thing we changed here is adding some extra
+        // velocity to the particles, which depend on an oscillating
+        // movement created with the Math.sin() function.
+        // You can change the frequency variable for fun results.
+        var frequency = frameCount * 0.1;
+        var extraVelocity = Math.sin(frequency) * 0.01;
+        // And changing velocityScalar is still fun, of course.
+        var velocityScalar = 0.01;
+        for (let i = 0; i < particles.length; i++) {
+            let p = particles[i];
+            p.x += (p.xVel * velocityScalar) + extraVelocity;
+            p.y += (p.yVel * velocityScalar) + extraVelocity;
+            let note = 0;
+            p.x = constrain(p.x, 0, 1);
+            p.y = constrain(p.y, 0, 1);
+            if (p.x == 0 || p.x == 1) {
+                p.xVel *= -1;
+                note = 1;
+            }
+            if (p.y == 0 || p.y == 1) {
+                p.yVel *= -1;
+                note = 1;
+            }
+            notesToSend += note;
+            // The second thing we changed here is creating two arrays of
+            // notes, which both create a different chord.
+            // The chord that is selected depends on the oscillation
+            // that we created above.
+            let notes = (Math.sin(frequency) > 0) ? random([2, 5, 10]) : random([0, 3, 7]);
+            let size = 3 + (6 * note);
+            ellipse(p.x * 600, p.y * 600, size);
+            if (note && notesToSend <  5) {
+                var msgToSend = {
+                    address: "/bouncy",
+                    args: [{
+                        type: "f",
+                        value: notes
+                    }]
+                };
+                socket.emit('msgToSCD', msgToSend);
+            }
+        }
+    };
+
+    // This block resets the particles array, fills it with more
+    // particles, and animates them with a chaotic function.
+    particles = [];
+    let n = 50;
+    for (let x = 0; x < n; x++) {
+        for (let y = 0; y <  n; y++) {
+            let p = {
+                x: x / n,
+                xVel: 1,
+                y: y / n,
+                yVel: 1
+            };
+            particles.push(p);
+        }
+    }
+    draw = function() {
+        background(180, 200, 200);
+        var notesToSend = 0;
+        var frequency = frameCount * 0.1;
+        var extraVelocity = Math.sin(frequency) * 0.01;
+        var velocityScalar = 0.001;
+        for (let i = 0; i < particles.length; i++) {
+            let p = particles[i];
+            // This is a chaotic function because its input is the previous
+            // state of the function, resulting in chaotic feedback and
+            // attractors.
+            p.x += Math.cos(p.x * p.y * 20) * p.xVel * velocityScalar;
+            p.y += Math.sin(p.x * 20) * p.yVel * velocityScalar;
+            let note = 0;
+            p.x = constrain(p.x, 0, 1);
+            p.y = constrain(p.y, 0, 1);
+            if (p.x == 0 || p.x == 1) {
+                p.xVel *= -1;
+                note = 1;
+            }
+            if (p.y == 0 || p.y == 1) {
+                p.yVel *= -1;
+                note = 1;
+            }
+            notesToSend += note;
+            let notes = (Math.sin(frequency) > 0) ? random([2, 5, 10]) : random([0, 3, 7]);
+            let size = 3 + (18 * note);
+            ellipse(p.x * 600, p.y * 600, size);
+            if (note && notesToSend <  5) {
+                var msgToSend = {
+                    address: "/bouncy",
+                    args: [{
+                        type: "f",
+                        value: notes
+                    }]
+                };
+                socket.emit('msgToSCD', msgToSend);
+            }
+        }
+    };
+
+    // The bracket below is the end of the "always false" if statement.
+}
---
+++ examples/bouncing-particles/sketch.js
@@ -0,0 +1,114 @@
+let looping = true;
+let keysActive = true;
+let socket;
+
+let particles = [];
+
+function setup() {
+    // socket.io allows the communication between the Web client
+    // and the Node.js server.
+    socket = io.connect('http://localhost:8080');
+    pixelDensity(1);
+    frameRate(30);
+    createCanvas(600, 600);
+    fill(0);
+    noStroke();
+    // Right below, we create 100 instances of a simple bouncing particle object
+    // and push them to the particles array. 
+    // Each particle has x and y properties for their positions, 
+    // and xVel and yVel properties for their velocities.
+    // Note that we use the native Math.random() JavaScript function
+    // to get a floating point number between 0 and 1, and we also use
+    // the p5.js random() function with an array as an argument. 
+    // When this function gets an array as an argument, 
+    // it returns a random element from the array.
+    for (let i = 0; i < 100; i++) {
+        let p = {
+            x: Math.random(),
+            xVel: Math.random() * random([-1, 1]),
+            y: Math.random(),
+            yVel: Math.random() * random([-1, 1])
+        };
+        particles.push(p);
+    }
+}
+
+draw = function() {
+    background(180, 200, 200);
+    // We set a number that scales the velocity of the particles.
+    // Changing this scalar while the sketch is running is a lot of fun.
+    // The bigger this number is, the faster the particles will go.
+    var velocityScalar = 0.02;
+    // The notesToSend variable will be used to count the amount
+    // of particles that bounce within a single draw() loop.
+    // We will use this number to limit the amount of
+    // notes that can be sent in a single loop.
+    var notesToSend = 0;
+    // We loop through the particles array
+    // in order to the draw the particles.
+    for (let i = 0; i < particles.length; i++) {
+        // We store the current particle in a variable with a short
+        // name just to help with the clarity of the code.
+        let p = particles[i];
+        // We add the current velocities to the current positions,
+        // but we first multiply the velocities by the scalar.
+        p.x += p.xVel * velocityScalar;
+        p.y += p.yVel * velocityScalar;
+        // We test if a particle has reached a wall of
+        // the rectangle. If it does, it will reverse the velocities
+        // and send a musical note to SuperCollider.
+        // If a note must be sent, the note variable will be set to 1;
+        let note = 0;
+        p.x = constrain(p.x, 0, 1);
+        p.y = constrain(p.y, 0, 1);
+        if (p.x == 0 || p.x == 1) {
+            p.xVel *= -1;
+            note = 1;
+        }
+        if (p.y == 0 || p.y == 1) {
+            p.yVel *= -1;
+            note = 1;
+        }
+        // We increment our count of notes.
+        notesToSend += note;
+        // The size of a particle is bigger if it is currently bouncing.
+        let size = 3 + (note * 12);
+        ellipse(p.x * 600, p.y * 600, size);
+        // We test if a note must be sent and if we have not reached
+        // our maximum amount of notes for a single draw() loop.
+        if (note == 1 && notesToSend < 5) {
+            // The object below will be sent to Supercollider.
+            // It has an address which SuperCollider will
+            // recognize and the value property within args will contain
+            // a random element from the array [0, 2, 4, 7, 11],
+            // thanks to the p5.js random() function.
+            var msgToSend = {
+                address: "/bouncy",
+                args: [{
+                    type: "f",
+                    value: random([0, 2, 4, 7, 11])
+                }]
+            };
+            // We send the object via Socket.io to the Node.js server,
+            // which will then relay it to SuperCollider via UDP.
+            socket.emit('msgToSCD', msgToSend);
+        }
+    }
+}
+
+function keyPressed() {
+    if (keysActive) {
+        if (keyCode === 32) {
+            if (looping) {
+                noLoop();
+                looping = false;
+            } else {
+                loop();
+                looping = true;
+            }
+        }
+        if (key == 'r' || key == 'R') {
+            window.location.reload();
+        }
+    }
+}
